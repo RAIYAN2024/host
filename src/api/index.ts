317d4b53d49df2a69a40107a35152adfbee420e3
Md Raiyan siddik <raiyansiddik2022@gmail.com>
2025-09-16 19:08:20 +0600

    uppdate all

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import serverless from "serverless-http";
 const app = express();
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ ip: req.ip, success: true, message: "Hello World" });
 });
-export const handler = serverless(app);
+export default app;
